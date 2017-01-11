4a84f8c01ad53ad83e0088f3134d14df0b5e39ed
huangchao <869158286@qq.com>
2017-01-11 11:32:17 +0800

    简单的2个链表合并，不创建新链表

---
+++ 21_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* result = new ListNode(-1);
+        ListNode* temp = result;
+        while (l1 != NULL || l2 != NULL) {
+            if (l2 == NULL || (l1 != NULL && l1->val < l2->val)) {
+                temp->next = l1;
+                l1 = l1->next;
+            } else {
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+        return result->next;
+    }
+};
