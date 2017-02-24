b61da4427fa1df434264e74491f15df005240fc5
hagho <869158286@qq.com>
2017-02-24 10:34:30 +0800

    十进制数字转罗马数字

---
+++ 12_Integer_to_Roman.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string result;
+        int num_table[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string roman_table[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int i = 0;
+        while (num > 0) {
+            for (; i < 13; i++) {
+                if (num >= num_table[i]) {
+                    break;
+                }
+            }
+            num -= num_table[i];
+            result += roman_table[i];
+        }
+        return result;
+    }
+};
