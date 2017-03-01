2a965b8afe5aa8fe5bbf134b361edf896653d33e
hagho <869158286@qq.com>
2017-03-01 15:20:07 +0800

    括号（）匹配，使用动态规划

---
+++ 22_Generate_Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> p[n + 1];
+        p[0] = vector<string>{""};
+        p[1] = vector<string>{"()"};
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0;j < i; j++) {
+                for (int k = 0 ; k < p[i - j - 1].size(); k++) {
+                    for (int l = 0;l < p[j].size(); l++) {
+                        p[i].push_back("(" + p[i-j-1][k]+ ")" + p[j][l]);
+                    }
+                }
+            }
+        }
+        return p[n];
+
+    }
+};
