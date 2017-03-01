aa68bfb0f62a7d2b6c25f26f455d1b9cb75461cb
hagho <869158286@qq.com>
2017-03-01 09:58:02 +0800

    简单的动态规划，计算数字二进制形式的1的个数

---
+++ 338_Counting_Bits.cpp
@@ -0,0 +1,11 @@
+//can use the builtin function __builtin_popcount
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> result(num + 1, 0);
+        for (int i = 1; i <= num; i++) {
+            result[i] = result[i >> 1] + (i & 1); 
+        }
+        return result;
+    }
+};
