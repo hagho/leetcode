b909c95f659df4b1fa4721e47b468c06081b59cc
hagho <869158286@qq.com>
2017-03-04 20:08:40 +0800

    简单的string问题

---
+++ 520_Detect_Capital[E].cpp
@@ -0,0 +1,19 @@
+#include <cctype>
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count = 0;
+        for (int i = 0; i < word.size(); i++) {
+            if (isupper(word[i])) {
+                count++;
+            }
+        }
+        if (word.size() == count || count == 0) {
+            return true;
+        } else if(count == 1 && isupper(word[0])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
