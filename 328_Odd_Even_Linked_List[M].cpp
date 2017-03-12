40f72993c23a0f9befb0329bc4d7828ce7000ff4
hagho <869158286@qq.com>
2017-03-12 21:30:11 +0800

    这道题是将奇数节点连接，偶数节点连接，然后拼接在一起，主要考察对链表操作的熟练以及链表间连接的熟悉

---
+++ 328_Odd_Even_Linked_List[M].cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head ==NULL) {
+            return head;
+        }
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* temp = even;
+        while (even && even->next) {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        } 
+        odd ->next = temp;
+        return head;
+    }
+};
