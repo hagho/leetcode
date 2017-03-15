85668c85a9e88ffb7118816b32dd8ad39c7e2ad4
hagho <869158286@qq.com>
2017-03-15 18:56:10 +0800

    简单题

--- 383_Ransom_Note[E].cpp
+++ 383_Ransom_Note[E].cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        
+        //set<char> r(ransomNote.begin(), ransomNote.end());
+        multiset<char> m(magazine.begin(), magazine.end());
+        for (int i = 0; i < ransomNote.size(); i++) {
+            auto itor = m.find(ransomNote[i]);
+            if (itor != m.end()) {
+                m.erase(itor);
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 };
