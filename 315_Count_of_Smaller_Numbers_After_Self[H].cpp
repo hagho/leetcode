f2084efbf980fcfe5d98a5ef9a88d39db9f5079a
hagho <869158286@qq.com>
2017-03-20 10:59:01 +0800

    归并排序求逆序，难度较大

--- 315_Count_of_Smaller_Numbers_After_Self[H].cpp
+++ 315_Count_of_Smaller_Numbers_After_Self[H].cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> results(n, 0);
+        vector<int> indices(n, 0);
+        iota(indices.begin(), indices.end(), 0);
+        merge_countSmailer(indices, 0, n, results, nums);
+        return results;
+    }
+private:
+    void merge_countSmailer(vector<int>& indices, int first, int last, vector<int>& results, vector<int>& nums) {
+        int count = last - first;
+        if (count > 1) {
+            int step = count / 2;
+            int mid = first + step;
+            merge_countSmailer(indices, first, mid, results, nums);
+            merge_countSmailer(indices, mid, last, results, nums);
+            vector<int> tmp;
+            tmp.reserve(count);
+            int idx1 = first;
+            int idx2 =  mid;
+            int semicount = 0;
+            while (idx1 < mid || idx2 < last) {
+                if ((idx2 == last) || ((idx1 < mid) && (nums[indices[idx1]] <= nums[indices[idx2]]))) {
+                    tmp.push_back(indices[idx1]);
+                    results[indices[idx1]] += semicount;
+                    ++idx1;
+                } else {
+                    tmp.push_back(indices[idx2]);
+                    semicount++;
+                    idx2++;
+                }
+            }
+            move(tmp.begin(), tmp.end(), indices.begin() + first);
+        }
+    }
+};
+/*class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
         vector<int> res;
         for (auto i = nums.end() -  1; i >= nums.begin(); i--) {
                 int  ans = 0;
@@ -17,4 +55,4 @@ public:
         }
         return res;
     }
-};
+};*/
