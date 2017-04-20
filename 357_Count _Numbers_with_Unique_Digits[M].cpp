2ebb55fd4f63598c11956fb998da970388bc570a
huangchao <869158286@qq.com>
2017-04-20 18:33:09 +0800

    递归，给你一个数字，求在这个位数范围内没有重复数字的数字总数

---
+++ 357_Count _Numbers_with_Unique_Digits[M].cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        return fun(n);
+    }
+private:
+    int fun(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 10;
+        } 
+        int res = 9;
+        int j = 9;
+        for(int i = 0; i < n - 1; i++) {
+            res *= j;
+            j--;
+        }
+        res += fun(n - 1);
+        return res;
+    }
+};
