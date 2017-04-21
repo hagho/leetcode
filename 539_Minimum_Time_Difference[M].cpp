ddb293a25a7717c0c7489ac26183a0b3a5fedd10
huangchao <869158286@qq.com>
2017-04-21 12:05:57 +0800

    算最小时间差，注意时间的特点，时间差的计算可以用到24*60=144=这个数字减少一个方向的计算，只用计算一个方向就可以，另一个方向就是1440-diff就能得到

---
+++ 539_Minimum_Time_Difference[M].cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        int n = timePoints.size();
+        sort(timePoints.begin(), timePoints.end());
+        int mindiff = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int diff = abs(timeDiff(timePoints[i], timePoints[( i + 1 ) % n]));
+            diff = min(diff, 1440 - diff);
+            mindiff = min(mindiff, diff);
+        }
+        return mindiff;
+    }
+
+private:
+    int timeDiff(string t1, string t2) {
+        int h1 = stoi(t1.substr(0, 2));
+        int h2 = stoi(t2.substr(0, 2));
+        int m1 = stoi(t1.substr(3, 2));
+        int m2 = stoi(t2.substr(3, 2));
+        return ( h2 - h1 ) * 60 + (m2 - m1);
+    }
+};
