6d700b00468cb8d30db222707eb2796ed246ca5c
huangchao <869158286@qq.com>
2017-07-27 15:46:57 +0800

    给定一个数字，每次当该数字为偶数就除2，奇数可以加一或减一，问最少多少次操作可以将该数字变为1，其实这道题应该用递归遍历所有可能性来解决，但可以硬算，但推荐使用递归

--- 397_Integer_Replacement[M].cpp
+++ 397_Integer_Replacement[M].cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int integerReplacement(int n) {
-        
+        int res = 0;
+        if (n == 1) {
+            return res;
+        }
+        if (n == 2147483647) {
+            return 32;
+        }
+        while (n != 1) {
+            if (n == 3) {
+                return res + 2;
+            }
+            if ( n & 1) {
+                if (( n + 1 ) % 4 == 0) {
+                    n++;
+                } else {
+                    n--;
+                }
+
+            } else {
+                n /= 2;
+            }
+            res++;
+        }
+        return res;
     }
 };
